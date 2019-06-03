3bc253bd6f1dc0672fb15f59af2b1fe2bc18300b
cedrick <cedrickdomingo048@gmail.com>
2019-06-04 02:46:11 +0800

    Updates

---
+++ precache-manifest.6a88125b43b437d5cea476437e35af66.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99bb47059bafb25f30a6a189b2e8fea0",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "21189477b4cf536c29c5",
+    "url": "/myportfolio/static/css/2.16a1cfe5.chunk.css"
+  },
+  {
+    "revision": "53415c6420046a08195a",
+    "url": "/myportfolio/static/css/main.d036afcd.chunk.css"
+  },
+  {
+    "revision": "21189477b4cf536c29c5",
+    "url": "/myportfolio/static/js/2.2572ff55.chunk.js"
+  },
+  {
+    "revision": "53415c6420046a08195a",
+    "url": "/myportfolio/static/js/main.2c2350fc.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "3a0a7a68ee187c9b1eb7455e025f7fca",
+    "url": "/myportfolio/static/media/12.employee_salary_logs.3a0a7a68.png"
+  },
+  {
+    "revision": "3ba1d78e9cce125ce7e08f0d8805cdfa",
+    "url": "/myportfolio/static/media/13.employee_settings_update.3ba1d78e.png"
+  },
+  {
+    "revision": "a8798a6fb866340a5b8e950e02776a57",
+    "url": "/myportfolio/static/media/14.employee_registration_desktop.a8798a6f.png"
+  },
+  {
+    "revision": "42fed0f2930fbc771accc777d960b8b0",
+    "url": "/myportfolio/static/media/15.global_configurations_update_desktop.42fed0f2.png"
+  },
+  {
+    "revision": "8a4267bb409b51169c5332d0529e07b1",
+    "url": "/myportfolio/static/media/2.home_desktop.8a4267bb.png"
+  },
+  {
+    "revision": "f5bbaa07736d8b85510a417dc52563e4",
+    "url": "/myportfolio/static/media/23.super_user_global_config_view.f5bbaa07.png"
+  },
+  {
+    "revision": "d73f9e7221fad14690c4fc93889619a0",
+    "url": "/myportfolio/static/media/26.regular_employee_salary_webapp.d73f9e72.png"
+  },
+  {
+    "revision": "580379313cc3c5c16947c0edbcbd84ea",
+    "url": "/myportfolio/static/media/30.api_gui.58037931.png"
+  },
+  {
+    "revision": "ce32e7d61b268c254333e06a3e2987db",
+    "url": "/myportfolio/static/media/6.graphs_desktop.ce32e7d6.png"
+  },
+  {
+    "revision": "8290831432d4856583c4f400d0d829d0",
+    "url": "/myportfolio/static/media/7.attendance_stats_graph.82908314.png"
+  },
+  {
+    "revision": "e9e9f778aeceeeb4b2d0c736d6b3fcdb",
+    "url": "/myportfolio/static/media/8.salary_stats_graph.e9e9f778.png"
+  },
+  {
+    "revision": "7e42c6439796dabf2cd35764135bf4c5",
+    "url": "/myportfolio/static/media/academia_desktop.7e42c643.png"
+  },
+  {
+    "revision": "6c16490936251657f3b63bb413c705ff",
+    "url": "/myportfolio/static/media/academia_desktop_2.6c164909.png"
+  },
+  {
+    "revision": "18156c2df2905d52415de146ee1dad16",
+    "url": "/myportfolio/static/media/academia_desktop_3.18156c2d.png"
+  },
+  {
+    "revision": "d8c8f579bfec9b697d3d97692059bc21",
+    "url": "/myportfolio/static/media/academia_thumbnail.d8c8f579.png"
+  },
+  {
+    "revision": "26603f1898814308dced061cf1895cc2",
+    "url": "/myportfolio/static/media/c_for_ced.26603f18.png"
+  },
+  {
+    "revision": "027c216b507943a853c18ebef27491ab",
+    "url": "/myportfolio/static/media/cctti_desktop.027c216b.png"
+  },
+  {
+    "revision": "4dc5c953a23c86e6704a26df364ca640",
+    "url": "/myportfolio/static/media/cctti_desktop_2.4dc5c953.png"
+  },
+  {
+    "revision": "220a35e44cc3d15a034f9966b7f9b8b5",
+    "url": "/myportfolio/static/media/cctti_desktop_3.220a35e4.png"
+  },
+  {
+    "revision": "6dca79cc94de47bae231b4e1f6b39a7d",
+    "url": "/myportfolio/static/media/cctti_desktop_4.6dca79cc.png"
+  },
+  {
+    "revision": "32a829087866d32f3f4211b17d86b597",
+    "url": "/myportfolio/static/media/cctti_thumbnail2.32a82908.png"
+  },
+  {
+    "revision": "39888c3b3ae646062ac0382962282237",
+    "url": "/myportfolio/static/media/cctti_thumbnail5.39888c3b.png"
+  },
+  {
+    "revision": "9e2c4dd71436c89b597e532fff2752d3",
+    "url": "/myportfolio/static/media/cctti_thumbnail_new.9e2c4dd7.png"
+  },
+  {
+    "revision": "d39ce2f97f962aa1f42ae2620f0fbc96",
+    "url": "/myportfolio/static/media/ced2.d39ce2f9.jpg"
+  },
+  {
+    "revision": "d7c5484d0456f57976587c55bc21d734",
+    "url": "/myportfolio/static/media/django.d7c5484d.png"
+  },
+  {
+    "revision": "0b463d2e5d530bef005f70cb5315d7ab",
+    "url": "/myportfolio/static/media/ems_thumbnail.0b463d2e.png"
+  },
+  {
+    "revision": "f5753318c7e4fa5affc93431cbabe48e",
+    "url": "/myportfolio/static/media/facerecog.f5753318.jpg"
+  },
+  {
+    "revision": "4749cb5b97ac863a04b0ebfd8b0a96a9",
+    "url": "/myportfolio/static/media/jwt.4749cb5b.png"
+  },
+  {
+    "revision": "9b3062d21193d25f4371dd5038157b06",
+    "url": "/myportfolio/static/media/jwtnodejs.9b3062d2.jpeg"
+  },
+  {
+    "revision": "47d714802a535a1446bcd01a39aa9f7b",
+    "url": "/myportfolio/static/media/memo_Desktop_2.47d71480.png"
+  },
+  {
+    "revision": "456f5a0b1788b227ebaaac41ff5958d3",
+    "url": "/myportfolio/static/media/memo_desktop.456f5a0b.png"
+  },
+  {
+    "revision": "0a5068a44cadefc7e8e0406a47af57a9",
+    "url": "/myportfolio/static/media/memo_mobiles.0a5068a4.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/myportfolio/static/media/nodejslogo.6dcf6999.png"
+  },
+  {
+    "revision": "caef7bd1f8b6a92eb184fb6773001c1b",
+    "url": "/myportfolio/static/media/opencv_logo.caef7bd1.png"
+  },
+  {
+    "revision": "5198f468d72eebca3b582d603b957929",
+    "url": "/myportfolio/static/media/password.5198f468.jpg"
+  },
+  {
+    "revision": "966b326179f5201652d65f991d97b669",
+    "url": "/myportfolio/static/media/phplogo.966b3261.png"
+  },
+  {
+    "revision": "b690510227d75cd1281d0e7907cb9e98",
+    "url": "/myportfolio/static/media/qtlogo.b6905102.png"
+  },
+  {
+    "revision": "2bee11a830bacc5ae9006df56b20c33a",
+    "url": "/myportfolio/static/media/reactlogo.2bee11a8.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/myportfolio/static/media/reduxlogo.8251fe76.png"
+  },
+  {
+    "revision": "bdd150a60c5cec9a9065e09e4eea41df",
+    "url": "/myportfolio/static/media/tango_with_django.bdd150a6.jpg"
+  },
+  {
+    "revision": "44aeeefb259a3129f26888809ff4e1be",
+    "url": "/myportfolio/static/media/university_corner122.44aeeefb.png"
+  },
+  {
+    "revision": "5fddced7f8d146def9f3f003d81d0d45",
+    "url": "/myportfolio/static/media/university_corner_2.5fddced7.png"
+  },
+  {
+    "revision": "236af1f4c2ea8e2df78b5cf22cc7d23a",
+    "url": "/myportfolio/static/media/university_corner_3.236af1f4.png"
+  },
+  {
+    "revision": "54dae307c78a2a07f15e06a619b6811e",
+    "url": "/myportfolio/static/media/university_corner_3.54dae307.jpg"
+  }
+]);
